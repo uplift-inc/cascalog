6df0117f2f6a05ffa244bf45c0970b97831b2f5c
pavanred <pavankumar.kh@gmail.com>
2016-03-11 13:45:17 -0800

    clj 1.7 udpate

--- cascalog-more-taps/project.clj
+++ cascalog-more-taps/project.clj
@@ -6,13 +6,16 @@
   :description "More taps for Cascalog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :javac-options ["-target" "1.7" "-source" "1.7"]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :jar-exclusions [#"\.java$"]
   :repositories {"conjars.org" "http://conjars.org/repo"}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :provided {:dependencies [[cascalog/cascalog-core ~VERSION]
                                        [org.apache.hadoop/hadoop-core ~HADOOP-VERSION]]}
              :dev {:plugins [[lein-midje "3.1.3"]]
