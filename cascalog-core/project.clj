92b6102806601671be7cbaef62ace7b77b643ae2
pavanred <pavankumar.kh@gmail.com>
2016-03-10 17:02:02 -0800

    change to cascading 3.0 and hadoop2 jar

--- cascalog-core/project.clj
+++ cascalog-core/project.clj
@@ -1,7 +1,7 @@
 (def ROOT-DIR (subs *file* 0 (- (count *file*) (count "project.clj"))))
 (def HADOOP-VERSION (-> ROOT-DIR (str "/../HADOOP-VERSION") slurp))
 (def VERSION (-> ROOT-DIR (str "/../VERSION") slurp))
-(def CC-VERSION (or (System/getenv "CASCALOG_CASCADING_VERSION") "2.5.3"))
+(def CC-VERSION (or (System/getenv "CASCALOG_CASCADING_VERSION") "3.0.0"))
 
 (defproject cascalog/cascalog-core VERSION
   :description "Cascalog core libraries."
@@ -21,7 +21,7 @@
                  [org.clojure/tools.macro "0.1.2"]
                  [log4j "1.2.16"]
                  [org.slf4j/slf4j-log4j12 "1.6.6"]
-                 [cascading/cascading-hadoop ~CC-VERSION
+                 [cascading/cascading-hadoop2-mr1 ~CC-VERSION
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]
                  [com.twitter/chill-hadoop "0.3.5"]
